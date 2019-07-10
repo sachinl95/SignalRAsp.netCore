07d20e9d858248a8fc5f61b0bb94b9352553f00e
sachinL95 <sachin.lahiru@gmail.com>
2019-07-10 11:40:12 +0530

    complte app

---
+++ SignalRHub/Controllers/MessageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRHub.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Message")]
+    public class MessageController : Controller
+    {
+        private IHubContext<NotifyHub, ITypedHubClient> _hubContext;
+
+        public MessageController(IHubContext<NotifyHub, ITypedHubClient> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public string Post([FromBody]Message msg)
+        {
+            string retMessage = string.Empty;
+            try
+            {
+                _hubContext.Clients.All.BroadcastMessage(msg.Type, msg.Payload);
+                retMessage = "Success";
+            }
+            catch (Exception e)
+            {
+                retMessage = e.ToString();
+            }
+            return retMessage;
+        }
+    }
+}
